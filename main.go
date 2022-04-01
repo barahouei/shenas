e842de77c9e7fed3def5f7067baa382e8a21a01e
barahouei <abdulghanibarahouei@gmail.com>
2022-04-01 18:10:25 +0430

    Basic message handling established for start.

--- main.go
+++ main.go
@@ -1,9 +1,69 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var entryKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("تعیین یا ویرایش سوالات و جواب‌ها"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("مشاهده سوالات و جواب‌ها"),
+	),
+
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("جواب‌های دوست‌هات"),
+	),
+
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("تنظیم اسم مستعار"),
+	),
+)
+
+func errorChecking(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func messageHandling(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel) {
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+
+		switch update.Message.Text {
+		case "start":
+			msg.ReplyMarkup = entryKeyboard
+		case "close":
+			msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+		}
+
+		_, err := bot.Send(msg)
+
+		errorChecking(err)
+
+	}
+}
+
 func main() {
-	fmt.Println("Hello friend!")
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	errorChecking(err)
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	messageHandling(bot, updates)
 }
