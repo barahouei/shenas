8c31e9f3499578006309518a7150000a2a75d7b4
barahouei <abdulghanibarahouei@gmail.com>
2022-04-14 20:52:12 +0430

    Changed set answer for question buttons to a two button in a row pattern.

--- keyboards.go
+++ keyboards.go
@@ -37,21 +37,57 @@ var backToEntry = tgbotapi.NewInlineKeyboardMarkup(
 	),
 )
 
+// //This functions gets a map of answers and create buttons for them.
+// func inlineButtons(opts map[string]string) [][]tgbotapi.InlineKeyboardButton {
+// 	var btns [][]tgbotapi.InlineKeyboardButton
+
+// 	for k, v := range opts {
+// 		key := "ans" + k
+// 		btns = append(btns, []tgbotapi.InlineKeyboardButton{
+// 			{
+// 				Text:         v,
+// 				CallbackData: &key,
+// 			},
+// 		})
+// 	}
+
+// 	return btns
+// }
+
 //This functions gets a map of answers and create buttons for them.
 func inlineButtons(opts map[string]string) [][]tgbotapi.InlineKeyboardButton {
-	var btns [][]tgbotapi.InlineKeyboardButton
+	var rows [][]tgbotapi.InlineKeyboardButton
+	var btn []tgbotapi.InlineKeyboardButton
+	i := 1
+	q := 1
 
 	for k, v := range opts {
+		j := len(opts)
 		key := "ans" + k
-		btns = append(btns, []tgbotapi.InlineKeyboardButton{
-			{
-				Text:         v,
-				CallbackData: &key,
-			},
-		})
+
+		btn = append(btn, tgbotapi.NewInlineKeyboardButtonData(v, key))
+
+		if i == 2 {
+			rows = append(rows, btn)
+
+			btn = []tgbotapi.InlineKeyboardButton{}
+			i = 0
+		}
+
+		if j-q == 1 {
+			rows = append(rows, btn)
+			btn = []tgbotapi.InlineKeyboardButton{}
+		}
+
+		if j-q == 0 {
+			rows = append(rows, btn)
+			btn = []tgbotapi.InlineKeyboardButton{}
+		}
+		i++
+		q++
 	}
 
-	return btns
+	return rows
 }
 
 //This functions gets a map of answers and friends telegram ID and create buttons for them.
