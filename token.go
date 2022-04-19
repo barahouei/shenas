083928a9459fd89ff213b7c15bda3d1f32d51b1a
barahouei <abdulghanibarahouei@gmail.com>
2022-04-19 17:07:58 +0430

    Updated the process of creating a token for custom link.

--- token.go
+++ token.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-	"crypto/md5"
-	"fmt"
-	"io"
-	"strconv"
+	"math/rand"
 	"time"
 )
 
-//This function generate an unique and reusable token which can be used as user's custom link
-//or a security approch if it was needed.
-func token() string {
-	crutime := time.Now().Unix()
-	hash := md5.New()
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789"
 
-	io.WriteString(hash, strconv.FormatInt(crutime, 10))
-	token := fmt.Sprintf("%x", hash.Sum(nil))
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+//This function creates a random string which contains character and number and returns that random string.
+func randomString(length int) string {
+	s := make([]byte, length)
+
+	for i := range s {
+		s[i] = charset[seededRand.Intn(len(charset))]
+	}
+
+	return string(s)
+}
+
+//This function generate an unique and reusable token which can be used as user's custom link
+func token() string {
+	token := randomString(8)
 
 	return token
 }
